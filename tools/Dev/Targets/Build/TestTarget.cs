c4b8760f6dfc550412a25bc4e7b4464de25abc88
Lance Craig <lancra@outlook.com>
2025-09-10 05:02:29 -0400

    Add test target

---
+++ tools/Dev/Targets/Build/TestTarget.cs
@@ -0,0 +1,50 @@
+namespace __PROJECT__.Dev.Targets.Build;
+
+internal sealed class TestTarget : ITarget
+{
+    private static readonly TestSuite[] Suites =
+    [
+        new(
+            TargetKeys.TestIntegration,
+            "Tests integrations between components of the application.",
+            [
+            ]),
+        new(
+            TargetKeys.TestUnit,
+            "Tests individual components of the application",
+            [
+            ]),
+    ];
+
+    public void Setup(Bullseye.Targets targets)
+    {
+        foreach (var suite in Suites)
+        {
+            targets.Add(
+                suite.Name,
+                suite.Description,
+                dependsOn: [TargetKeys.Dotnet],
+                forEach: suite.Projects,
+                Execute);
+        }
+
+        targets.Add(
+            TargetKeys.Test,
+            "Executes automated test suites.",
+            dependsOn: Suites.Select(suite => suite.Name)
+                .ToArray());
+    }
+
+    private static async Task Execute(TestProject project)
+    {
+        var testResultsPath = string.Format(null, ArtifactPaths.TestResultFormat, project.Name);
+        await DotnetCli
+            .Run(
+                $"test {project.Path}",
+                "--no-build",
+                "--collect \"XPlat Code Coverage\"",
+                "--logger trx",
+                $"--results-directory {testResultsPath}")
+            .ConfigureAwait(false);
+    }
+}
