4e4abe8f0921b12dd1044e620e71c3f34786145a
Lance Craig <lancra@outlook.com>
2025-09-10 04:59:41 -0400

    Add lint target

--- tools/Dev/Targets/TargetExtensions.cs
+++ tools/Dev/Targets/TargetExtensions.cs
@@ -6,7 +6,8 @@ namespace __PROJECT__.Dev.Targets;
 internal static class TargetExtensions
 {
     public static IServiceCollection AddTargets(this IServiceCollection services)
-        => services.AddTarget<DefaultTarget>();
+        => services.AddTarget<DefaultTarget>()
+        .AddTarget<LintTarget>();
 
     private static IServiceCollection AddTarget<
         [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TTarget>(
