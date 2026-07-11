00fb154f3987f64ef14821baacb117b3c7bb6d58
Lance Craig <lancra@outlook.com>
2026-07-11 12:14:09 -0400

    Support token population from parameter
    This enables token values to be provided with the Value parameter. If
    the intent is for all values to be provided in this manner, the NoPrompt
    switch can be set to fail if any required tokens are missed.

--- .template/scripts/populate-token-values.ps1
+++ .template/scripts/populate-token-values.ps1
@@ -4,16 +4,29 @@ Prompts for population of static token definition values in a specification.
 
 .DESCRIPTION
 Iterates through each token definition in the provided specification, prompting
-for a value. The resulting specification is finally rewritten back to the file.
+for a value if it is not provided as a parameter. The resulting specification is
+then rewritten back to the file.
 
 .PARAMETER Specification
 The source template specification to add token values to.
+
+.PARAMETER Value
+The token key-values to population in the target specification.
+
+.PARAMETER NoPrompt
+Specifies that a missing token value should trigger an error rather than
+prompting for input.
 #>
 
 [CmdletBinding()]
 param(
     [Parameter(Mandatory)]
-    [string] $Specification
+    [string] $Specification,
+
+    [Parameter()]
+    [hashtable] $Value = @{},
+
+    [switch] $NoPrompt
 )
 
 if (-not (Test-Path -Path $Specification)) {
@@ -23,6 +36,7 @@ if (-not (Test-Path -Path $Specification)) {
 $specificationInstance = Get-Content -Path $Specification |
     ConvertFrom-Json
 
+$missingValueKeys = @()
 $specificationInstance.tokens.PSObject.Properties |
     Select-Object -ExpandProperty Name |
     ForEach-Object {
@@ -32,23 +46,47 @@ $specificationInstance.tokens.PSObject.Properties |
         }
 
         $examplePrefix = $token.default ? 'default' : 'e.g.'
-        $prompt = "${_}:`n$($token.description) ($examplePrefix '$($token.example)')"
-        $userInput = ''
-        while (-not $userInput) {
-            $userInput = Read-Host -Prompt $prompt
+        $tokenValue = $Value[$_]
+        $tokenDefaultValue = $token.default ? $token.example : $null
 
-            if (-not $userInput) {
-                if ($token.default) {
-                    $userInput = $token.example
+        $promptRequired = -not $tokenValue
+        if ($promptRequired -and $NoPrompt) {
+            if ($tokenDefaultValue) {
+                $tokenValue = $tokenDefaultValue
+            } else {
+                $missingValueKeys += $_
+                return
+            }
+        }
+
+        $prompt = "${_}:`n$($token.description) ($examplePrefix '$($token.example)')"
+        while (-not $tokenValue) {
+            $tokenValue = Read-Host -Prompt $prompt
+
+            if (-not $tokenValue) {
+                if ($tokenDefaultValue) {
+                    $tokenValue = $tokenDefaultValue
                 } else {
-                    Write-Output "`e[33mPlease provide a value.`e[39m"
+                    Write-Output "$($PSStyle.Foreground.Yellow)Please provide a value.$($PSStyle.Reset)"
                 }
             }
         }
 
-        $specificationInstance.tokens.$_.value = $userInput
-        Write-Output ''
+        $specificationInstance.tokens.$_.value = $tokenValue
+
+        if ($promptRequired -and -not $NoPrompt) {
+            Write-Output ''
+        }
     }
+
+if ($missingValueKeys.Length -gt 0) {
+    Write-Output "$($PSStyle.Foreground.Red)Please provide the missing values below or execute with prompts enabled.$($PSStyle.Reset)"
+    $missingValueKeys |
+        ForEach-Object {
+            Write-Output $_
+        }
+    exit 1
+}
 
 $specificationInstance |
     ConvertTo-Json -Depth 3 |
